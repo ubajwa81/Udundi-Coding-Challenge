c0ddedc44d14b1e5bd1ad4086ecd46fa68b5ea9d
ubajwa81 <59077459+ubajwa81@users.noreply.github.com>
2025-07-03 01:11:54 +0500

    updated

--- assets/script.js
+++ assets/script.js
@@ -8,13 +8,13 @@ const showModal = (e) => {
     //openBtn.classList.add('scale-0', 'opacity-0');
     
     openBtn.classList.add("opacity-0","invisible");
-    setTimeout(() => {
-        
     modal.classList.remove("opacity-0","invisible","scale-0");
     content.classList.add("opacity-0","invisible");
     document.body.style.overflow = "hidden";
     gsap.fromTo(e.currentTarget.querySelector('svg'), {scale: 1}, {scale: 0.75, duration: 0.25, yoyo: true, repeat: 1, overwrite: true});
     modal.querySelector("button").focus();
+    setTimeout(() => {
+        
 
     }, 300);
 
@@ -26,12 +26,13 @@ const hideModal = () => {
     content.classList.remove("opacity-0","invisible");
     modal.classList.add("opacity-0","invisible","scale-0");
     document.body.style.overflow = "";
-
+    
+    openBtn.classList.remove("opacity-0","invisible");
+    openBtn.focus();    
     setTimeout(() => {
         // openBtn.classList.remove('scale-0', 'opacity-0');
         // openBtn.classList.add('scale-100', 'opacity-100');
-        openBtn.classList.remove("opacity-0","invisible");
-        openBtn.focus();    
+        
     }, 400);
 
 
